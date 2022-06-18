5e93a8445b522e5641fff7edcf346fdf86924d25
Vedant <vedantk60@gmail.com>
2022-06-19 00:03:48 +0530

    :tada: Initial commit
    Add initial project files.

---
+++ android/app/src/main/kotlin/com/example/gsoc_organizations/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.gsoc_organizations
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Organizations"),
+      ),
+      body: SingleChildScrollView(
+        child: Container(
+            child: const Center(
+          child: Text("GSOC Organizations....."),
+        )),
+      ),
+    );
+  }
+}
